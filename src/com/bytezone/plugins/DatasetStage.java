5408862db65c14178579d26ffd2104c88b4e2119
Denis Molony <dmolony@iinet.net.au>
2015-05-16 18:19:37 +1000

    created Document

--- src/com/bytezone/plugins/DatasetStage.java
+++ src/com/bytezone/plugins/DatasetStage.java
@@ -1,10 +1,7 @@
 package com.bytezone.plugins;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.prefs.Preferences;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -26,10 +23,6 @@ import com.bytezone.dm3270.plugins.ScreenField;
 
 public class DatasetStage extends Stage
 {
-  private static final String START_DATA = "***************************** Top of Dat"
-      + "a ******************************";
-  private static final String END_DATA = "**************************** Bottom of D"
-      + "ata ****************************";
   private final Preferences prefs = Preferences.userNodeForPackage (this.getClass ());
   private final WindowSaver windowSaver = new WindowSaver (prefs, this, "PluginDataset");
 
@@ -41,7 +34,6 @@ public class DatasetStage extends Stage
   private final TextField datasetLinesText;
   private final TextField datasetColumnsText;
 
-  private final Pattern p;
   private int totalLines;
   private int largestColumn;
   private final Font defaultFont;
@@ -49,16 +41,13 @@ public class DatasetStage extends Stage
   private boolean doesAuto;
   private boolean doesRequest;
 
+  private final Document document = new Document ();
+
   public DatasetStage ()
   {
     setTitle ("Dataset Display");
     setOnCloseRequest (e -> hide ());
     setTitle ("Dataset Display");
-
-    String pattern = "([A-Z0-9]{1,8}(\\.[A-Z0-9]{1,8})*)" // dataset name
-        + "(\\([A-Z0-9]{1,8}\\))?"                        // member name
-        + "( - [0-9]{2}\\.[0-9]{2})?";                    // editing data
-    p = Pattern.compile (pattern);
 
     doesRequest = true;
     doesAuto = false;
@@ -132,7 +121,7 @@ public class DatasetStage extends Stage
     if (editPosition > 0 && commandPosition > editPosition
         && scrollPosition > commandPosition + 1)
     {
-      EditorPage editorPage = new EditorPage (data, modifiableFields);
+      DocumentPage editorPage = new DocumentPage (data, modifiableFields);
 
       if (editorPage.rightColumn > largestColumn)
       {
@@ -239,131 +228,5 @@ public class DatasetStage extends Stage
   public void closing ()
   {
     windowSaver.saveWindow ();
-  }
-
-  public class EditorPage
-  {
-    String datasetName;
-    String memberName;
-    int firstLine = -1;
-    int lastLine;
-    boolean hasBeginning;
-    boolean hasEnd;
-    int leftColumn;
-    int rightColumn;
-    List<String> numbers = new ArrayList<> ();
-    List<String> lines = new ArrayList<> ();
-
-    public EditorPage (PluginData data, List<ScreenField> modifiableFields)
-    {
-      getDatasetName (data);
-      getColumns (data);
-
-      for (ScreenField sf : modifiableFields)
-        switch (sf.location.column)
-        {
-          case 1:
-            if (sf.length == 6 && sf.data.equals ("******"))
-            {
-              ScreenField nextField = data.getField (sf.sequence + 1);
-              if (nextField != null && nextField.isProtected && nextField.length >= 72)
-                if (nextField.data.equals (START_DATA))
-                  hasBeginning = true;
-                else if (nextField.data.equals (END_DATA))
-                  hasEnd = true;
-            }
-            else
-              numbers.add (sf.data);
-            break;
-
-          case 8:
-            lines.add (sf.data);
-            break;
-
-          case 14:      // command field
-          case 75:      // CSR/PAGE
-            break;
-
-          default:
-            System.out.printf ("column %d: %s%n", sf.location.column, sf.data);
-        }
-
-      if (lines.size () > 0)
-      {
-        firstLine = Integer.parseInt (numbers.get (0));
-        lastLine = Integer.parseInt (numbers.get (numbers.size () - 1));
-      }
-    }
-
-    private void getDatasetName (PluginData data)
-    {
-      datasetName = data.trimField (12);
-      Matcher m = p.matcher (datasetName);
-      if (m.matches ())
-      {
-        if (false)
-        {
-          System.out.println (m.groupCount ());
-          for (int i = 0; i <= m.groupCount (); i++)
-            System.out.printf ("%2d %s%n", i, m.group (i));
-        }
-
-        datasetName = m.group (1);
-        if (m.groupCount () >= 3)
-        {
-          String name = m.group (3);
-          memberName = name.substring (1, name.length () - 1);
-        }
-        else
-          memberName = "";
-      }
-    }
-
-    private void getColumns (PluginData data)
-    {
-      int columnsPosition = findField ("Columns", data);        // may not exist
-      if (columnsPosition < 0)
-        return;
-
-      ScreenField columnField = data.getField (columnsPosition);
-      if (columnField.length != 7 || columnField.isModifiable)
-        return;
-
-      String col1 = data.trimField (columnsPosition + 1);
-      String col2 = data.trimField (columnsPosition + 2);
-
-      try
-      {
-        leftColumn = Integer.parseInt (col1);
-        rightColumn = Integer.parseInt (col2);
-      }
-      catch (NumberFormatException e)
-      {
-        leftColumn = 0;
-        rightColumn = 0;
-      }
-    }
-
-    @Override
-    public String toString ()
-    {
-      StringBuilder text = new StringBuilder ();
-
-      text.append (String.format ("Dataset name .... %s%n", datasetName));
-      text.append (String.format ("Member name ..... %s%n", memberName));
-      text.append (String.format ("Lines ........... %d%n", lines.size ()));
-      text.append (String.format ("First line ...... %d%n", firstLine));
-      text.append (String.format ("Last line ....... %d%n", lastLine));
-      text.append (String.format ("Has first ....... %s%n", hasBeginning));
-      text.append (String.format ("Has last ........ %s%n", hasEnd));
-      text.append (String.format ("Left column ..... %d%n", leftColumn));
-      text.append (String.format ("Right column .... %d%n", rightColumn));
-      text.append ("\n");
-
-      for (int i = 0; i < lines.size (); i++)
-        text.append (String.format ("%s %s%n", numbers.get (i), lines.get (i)));
-
-      return text.toString ();
-    }
   }
 }
---
+++ src/com/bytezone/plugins/Document.java
@@ -0,0 +1,19 @@
+package com.bytezone.plugins;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Document
+{
+  List<DocumentPage> pages = new ArrayList<> ();
+
+  public Document ()
+  {
+
+  }
+
+  public void addDocumentPage (DocumentPage documentPage)
+  {
+
+  }
+}
